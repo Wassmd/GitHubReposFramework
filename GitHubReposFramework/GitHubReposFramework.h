9ca4e3487b255a0f0c658fc0262a1e8edf6b5a7b
Wasim <mohammed.wasimuddin@payback.net>
2020-06-26 21:04:29 +0200

    Project setup

---
+++ GitHubReposFramework/GitHubReposFramework.h
@@ -0,0 +1,19 @@
+//
+//  GitHubReposFramework.h
+//  GitHubReposFramework
+//
+//  Created by Mohammed Wasimuddin on 26.06.20.
+//  Copyright © 2020 wasim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GitHubReposFramework.
+FOUNDATION_EXPORT double GitHubReposFrameworkVersionNumber;
+
+//! Project version string for GitHubReposFramework.
+FOUNDATION_EXPORT const unsigned char GitHubReposFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitHubReposFramework/PublicHeader.h>
+
+
