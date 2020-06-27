5e585d7015f9d0e3fcb8ebace05e8e7b748fbcc9
Wasim <mohammed.wasimuddin@payback.net>
2020-06-27 06:03:12 +0200

    Add documentation

--- GitHubReposFramework/GithubReposService.h
+++ GitHubReposFramework/GithubReposService.h
@@ -5,8 +5,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GithubReposService : NSObject
 
+/// A convenience constructor that initializes with Loader
+/// @param loader Network Loader
 - (instancetype)initWithLoader:(Loader*)loader;
 
+///
+/// @param userName Github username i.e fabpot
+/// @param successBlock returns the array of Repository belonging to the username
+/// @param errorBlock return error
  - (void)getGithubReposWithUsername:(NSString*)userName
                       SuccessBlock:(void(^)(NSArray *repositories))successBlock
                         errorBlock:(void(^)(NSError *error))errorBlock;
